850a39bf54191736f13fc3711273c1cd7c033266
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-18 10:14:13 +0900

    Initial Commit

---
+++ Shared/FyloDarkThemeLandingPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  FyloDarkThemeLandingPageApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/05/18.
+//
+
+import SwiftUI
+
+@main
+struct FyloDarkThemeLandingPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
