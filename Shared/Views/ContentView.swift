d84dbc15959215024848c0f95e3b7bfff976dd08
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-18 10:33:34 +0900

    Add the header view.

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
             VStack {
                 TopNavigationView()
                 IntroView()
+                HeaderView()
             }
         }
     }
---
+++ Shared/Views/HeaderView.swift
@@ -0,0 +1,35 @@
+//
+//  HeaderView.swift
+//  Fylo Dark Theme Landing Page (iOS)
+//
+//  Created by Jinwook Kim on 2022/05/18.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        VStack(spacing: 24) {
+            Text("All your files in one secure location, accessible anywhere.")
+                .font(.largeTitle.bold())
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .fixedSize(horizontal: false, vertical: true)
+            Text("Fylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends, family, and co-workers.")
+                .font(.title3)
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .lineSpacing(5)
+                .fixedSize(horizontal: false, vertical: true)
+        }
+        .padding()
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView()
+            .background(Color("BackgroundColor"))
+            .previewLayout(.sizeThatFits)
+    }
+}
